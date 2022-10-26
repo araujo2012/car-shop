ef28092d56a29f33d93b98c0b80419e258ceb0c3
araujo2012 <araujo2012@hotmail.com>
2022-10-25 23:47:20 -0300

    feat: CarRoute

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express';
+import CarRoute from './routes/CarRoute';
 
 const app = express();
+app.use(CarRoute);
 
 export default app;
---
+++ src/routes/CarRoute.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import CarController from '../controllers/CarController';
+import CarModel from '../models/CarModel';
+import CarService from '../services/CarService';
+
+const CarRoute = Router();
+
+const carModel = new CarModel();
+const carService = new CarService(carModel);
+const carController = new CarController(carService);
+
+CarRoute.post('/cars', (req, res) => carController.create(req, res));
+CarRoute.get('/cars/:id', (req, res) => carController.readOne(req, res));
+
+export default CarRoute;
