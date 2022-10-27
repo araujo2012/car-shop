bbbd404860e023d173e82463a8b4ee5b2fe32157
araujo2012 <araujo2012@hotmail.com>
2022-10-27 11:17:59 -0300

    feat: VehicleZodSchema

--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -1,3 +1,5 @@
+import { z } from 'zod';
+
 export interface IVehicle {
   model: string,
   year: number,
@@ -5,3 +7,11 @@ export interface IVehicle {
   status?: boolean,
   buyValue: number,
 }
+
+export const VehicleZodSchema = z.object({
+  model: z.string(),
+  year: z.number(),
+  color: z.string(),
+  status: z.boolean().optional(),
+  buyValue: z.number(),
+});
