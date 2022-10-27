fd98ff516d15348cf6a24500c3cc9996a6a3007d
araujo2012 <araujo2012@hotmail.com>
2022-10-27 11:17:49 -0300

    feat: CarZodSchema

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -1,6 +1,12 @@
-import { IVehicle } from './IVehicle';
+import { z } from 'zod';
+import { IVehicle, VehicleZodSchema } from './IVehicle';
 
 export interface ICar extends IVehicle {
   doorsQty: number,
   seatsQty: number,
 }
+
+export const CarZodSchema = VehicleZodSchema.extend({
+  doorsQty: z.number(),
+  seatsQty: z.number(),
+});
