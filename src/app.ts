08598f3808f03ba1af75c6d4dcd3234abb9f08fa
araujo2012 <araujo2012@hotmail.com>
2022-10-29 14:44:49 -0300

    feat: errorhandler

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import 'express-async-errors';
+import errorHandler from './middlewares/error';
 import CarRoute from './routes/CarRoute';
 
 const app = express();
+app.use(express.json());
 app.use(CarRoute);
+app.use(errorHandler);
 
 export default app;
